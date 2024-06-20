28622364651f426c368c44ceb1cb9cc27c2a69e0
pet-z <pet_zed@rambler.ru>
2024-06-21 00:09:04 +0300

    Работающая версия бота. Оно разворачивается и запускается.

---
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -0,0 +1,32 @@
+package ru.relex.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Value("${bot.name}")
+    private String botName;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        System.out.println(originalMessage.getText());
+    }
+}
